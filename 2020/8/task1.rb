991161ef0b80c47bbb2fd99d2032498e359be05e
Simon Hyttfors <simonhyttfors@gmail.com>
2020-12-08 15:43:30 +0100

    Cure OCD

--- 2020/8/task1.rb
+++ 2020/8/task1.rb
@@ -1,7 +1,7 @@
 @instructions = File.readlines('prog').map(&:chomp).map { |i| { ins: i.split[0], arg: i.split[1].to_i }}
 def calc_acc(line, acc = 0, processed_lines = [])
-  return acc if processed_lines.include? line
   i = @instructions[line]
+  return acc if processed_lines.include? line
   return calc_acc(line+1, acc, processed_lines.push(line)) if i[:ins] == 'nop'
   return calc_acc(line+1, acc+i[:arg], processed_lines.push(line)) if i[:ins] == 'acc'
   return calc_acc(line+i[:arg], acc, processed_lines.push(line)) if i[:ins] == 'jmp'
