ff91ba26361381b3be2eebe8edd4186ed0b2d0f6
Simon Hyttfors <simonhyttfors@gmail.com>
2021-12-06 20:10:26 +0100

    Add solution to first task, day 4

---
+++ 2021/04/task1.rb
@@ -0,0 +1,23 @@
+@bingo = File.readlines('bingo').map(&:chomp).reject &:empty?
+@numbers = @bingo.first.split(',').map &:to_i
+@boards = @bingo[1..-1].map { |l| l.split.map &:to_i }.each_slice(5).map { |a, b, c, d, e| [a, b, c, d, e] }
+
+def find_bingo
+  @numbers.each.with_index do |number, i|
+    @boards.each.with_index do |board, j|
+      board.each.with_index do |row, k|
+        found = false
+
+        row.each.with_index do |num, l|
+          @boards[j][k][l] = nil if num == number
+          
+          if @boards[j][k].all?(nil) || @boards[j].map { |r| r[l] }.all?(nil)
+            return @boards[j].flatten.reject(&:nil?).sum*number
+          end
+        end
+      end
+    end
+  end
+end
+
+pp find_bingo
